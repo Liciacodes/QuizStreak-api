143d9f67e8e9e6a9ef11b0526ea1f8b011e9ac97
Liciacodes <udosenfelicia@gmail.com>
2026-07-01 13:02:14 +0100

    renamed url

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
      "http://localhost:5173",
-    "https://quiz-streak.vercel.app"
+    "https://quiz-streak-client.vercel.app"
   ],
   credentials: true,
 }));
